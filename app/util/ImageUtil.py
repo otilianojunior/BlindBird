3cd3a060a18629c684be17ec839ab9748aef0a13
Otiliano Junior <otiliano.rodrigues@veri.com.br>
2023-09-18 15:52:49 -0300

    Feat: imageUtil

--- app/util/ImageUtil.py
+++ app/util/ImageUtil.py
@@ -4,17 +4,17 @@ import os
 class ImageUtil:
     def file_exists(self, image_name):
         try:
-            current_dir = os.path.dirname(__file__)
-            image_path = os.path.join(current_dir, 'app/assets/imgs/', image_name)
+            base_dir = os.path.dirname(os.path.abspath(__file__))
+            image_path = os.path.join(base_dir, 'app/assets/imgs', image_name)
             return os.path.exists(image_path)
         except Exception as ex:
-            raise Exception(f"Este aqrquivo não foi encontrado: {ex}")
+            raise Exception(f"Erro ao verificar a existência do arquivo: {ex}")
 
     def load_image(self, image_name):
         try:
             if self.file_exists(image_name):
-                current_dir = os.path.dirname(__file__)
-                image_path = os.path.join(current_dir, 'app/assets/imgs/', image_name)
+                base_dir = os.path.dirname(os.path.abspath(__file__))
+                image_path = os.path.join(base_dir, 'app/assets/imgs/', image_name)
                 return image_path
             else:
                 raise FileNotFoundError(f"Arquivo de imagem não encontrado: {image_name}")
@@ -26,7 +26,7 @@ if __name__ == "__main__":
     util = ImageUtil()
     image_name = "pipe.png"
     try:
-        loaded_image = util.load_image(image_name)
+        loaded_image = util.file_exists(image_name)
         print(loaded_image)
     except Exception as ex:
         print(f"Erro: {ex}")
